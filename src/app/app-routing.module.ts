b97ca593da10c338954632c31f5bf752673f0302
sameerkhan78600 <sameerkhan4321087@gmail.com>
2023-06-25 18:54:46 +0530

    fsdghdfghj

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProfileComponent } from './routing_for_navbar/profile/profile.component';
 import { AppComponent } from './app.component';
 import { MissionComponent } from './routing_for_navbar/mission/mission.component';
+import { HomeComponent } from './home/home.component';
 const routes: Routes = [
 {path:'profile',component:ProfileComponent},
 {path:'mission',component:MissionComponent},
+{ path : '', component :HomeComponent},
 
 
 
