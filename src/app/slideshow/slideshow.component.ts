024cf5153d84fba83fb2c8e13d4476815d546bbf
sameerkhan78600 <sameerkhan4321087@gmail.com>
2023-07-03 14:21:48 +0530

    safdafgs

--- src/app/slideshow/slideshow.component.ts
+++ src/app/slideshow/slideshow.component.ts
@@ -3,11 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-slideshow',
   templateUrl: './slideshow.component.html',
-  styleUrls: ['./slideshow.component.css']
+  styleUrls: ['./slideshow.component.css'],
 })
 export class SlideshowComponent {
-  message1: string = 'Title of schemes/ services/ event/ activity appears here.                 ';
-  
-  message2: string = 'Official notifications for the media about an event or an official documents adhering to the concern.';
-  message3: string = 'Official notifications for the media about an event or an official documents adhering to the concern.';
+  messages1: string[] = [
+    'Admission Notice 2023-2024',
+    'Compilation of Research Studies 2019-2022',
+    'Additional message 1',
+    '',
+  ];
+  messages2: string[] = [
+    'State Level Teachers Conference',
+    'On site support to school under BRC ,Rakdong,East Sikkim',
+    'Coordination meeting of Teacher Education Institutes(DIET)',
+    'School Internship Programs for 2nd and 4th Semester Students Teachers',
+    'Consultative meeting with heads of schools',
+    'Pre-SIP Preparatory Workshop for second year pre service Student Teacher',
+  ];
+  messages3: string[] = ['STAFF SELECTION COMMISSION PUBLIC PORTAL', 'Employment News of Sikkim', 'Important News Portal of India'];
 }
