3e6897f9d634de16cf2761abbc65651513030827
sameerkhan78600 <sameerkhan4321087@gmail.com>
2023-08-02 19:35:16 +0530

    fgfdh

--- src/app/routing_for_navbar/media/photoss/photoss.component.ts
+++ src/app/routing_for_navbar/media/photoss/photoss.component.ts
@@ -11,13 +11,13 @@ export class PhotossComponent {
     { url: '/assets/img/MAAM2.jpeg', caption: 'Photo 2' },
     { url: '/assets/img/MAAM3.jpeg', caption: 'Photo 3' },
     { url: '/assets/img/MAAM9.jpeg', caption: 'Photo 4' },
-    { url: '/assets/img/MAAM10.jpeg', caption: 'Photo 5' },
-    { url: '/assets/img/MAAM11.jpeg', caption: 'Photo 6' },
-    { url: '/assets/img/MAAM12.jpeg', caption: 'Photo 7' },
-    { url: '/assets/img/MAAM13.jpeg', caption: 'Photo 8' },
-    { url: '/assets/img/MAAM14.jpeg', caption: 'Photo 9' },
-    { url: '/assets/img/MAAM1.jpeg', caption: 'Photo 10' },
-    { url: '/assets/img/MAAM11.jpeg', caption: 'Photo 11' },
-    { url: '/assets/img/MAAM2.jpeg', caption: 'Photo 12' }
+    { url: '/assets/img/new.jpeg', caption: 'Photo 5' },
+    { url: '/assets/img/new1.jpeg', caption: 'Photo 6' },
+    { url: '/assets/img/new2.jpeg', caption: 'Photo 7' },
+    { url: '/assets/img/new3.jpeg', caption: 'Photo 8' },
+    { url: '/assets/img/new4.jpeg', caption: 'Photo 9' },
+    { url: '/assets/img/new5.jpeg', caption: 'Photo 10' },
+    { url: '/assets/img/new6.jpeg', caption: 'Photo 11' },
+    { url: '/assets/img/new7.jpeg', caption: 'Photo 12' }
   ];
 }
