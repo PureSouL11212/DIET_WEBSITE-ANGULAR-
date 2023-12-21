0aad7eb4a700b014761aeaae41bd93334cf97a26
ITS_A_PURE_SOUL <109498451+PureSouL11212@users.noreply.github.com>
2023-12-21 18:23:27 +0530

    asddasd

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  isSmallScreen = false; // Initially, the menu is shown
 
+  constructor() { }
+
+  ngOnInit() {
+    this.checkScreenSize();
+    window.addEventListener('resize', () => {
+      this.checkScreenSize();
+    });
+  }
+
+  checkScreenSize() {
+    this.isSmallScreen = window.innerWidth < 768; // Adjust the threshold as needed
+  }
+
+  toggleMenu() {
+    this.isSmallScreen = !this.isSmallScreen;
+  }
 }
--- src/app/startbar/startbar.component.ts
+++ src/app/startbar/startbar.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+// import { Component } from '@angular/core';
 
+// @Component({
+//   selector: 'app-startbar',
+//   templateUrl: './startbar.component.html',
+//   styleUrls: ['./startbar.component.css']
+// })
+// export class StartbarComponent {
+
+// }
+import { Component, HostListener } from '@angular/core';
 @Component({
   selector: 'app-startbar',
-  templateUrl: './startbar.component.html',
-  styleUrls: ['./startbar.component.css']
+    templateUrl: './startbar.component.html',
+    styleUrls: ['./startbar.component.css']
 })
 export class StartbarComponent {
+  isSmallScreen: boolean = false;
 
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any): void {
+    this.isSmallScreen = window.innerWidth <= 950; // Adjust the threshold as needed
+  }
+  toggleLanguage(): void {
+    this.isSmallScreen = !this.isSmallScreen;
+  }
 }
